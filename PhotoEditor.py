6c8c450a85e60405d41100cb97ecbea41d0a1102
noahsali <noahsali@woodland.on.ca>
2022-11-29 13:52:17 -0500

    First Code on Project

--- PhotoEditor.py
+++ PhotoEditor.py
@@ -2,3 +2,25 @@ from PIL import Image
 im = Image.open("shreks.jpg")
 print(im.format, im.size, im.mode)
 im.show()
+
+import os, sys
+from PIL import Image
+
+size = (128, 128)
+
+for infile in sys.argv[1:]:
+    outfile = os.path.splitext(infile)[0] + ".thumbnail"
+    if infile != outfile:
+        try:
+            with Image.open(infile) as im:
+                im.thumbnail(size)
+                im.save(outfile, "JPEG")
+        except OSError:
+            print("cannot create thumbnail for", infile)
+
+box = (100, 100, 400, 400)
+region = im.crop(box)
+
+region = region.transpose(Image.Transpose.ROTATE_180)
+im.paste(region, box)
+
