e6d1108bb3200685d6d896ccbf67fa60494ecc03
noahsali <noahsali@woodland.on.ca>
2022-12-05 10:32:36 -0500

    First Code on Project

--- PhotoEditor.py
+++ PhotoEditor.py
@@ -1,26 +1,14 @@
 from PIL import Image
-im = Image.open("shreks.jpg")
-print(im.format, im.size, im.mode)
-im.show()
+print("pick a photo (Shrek, Ronaldo):")
 
-import os, sys
-from PIL import Image
+x = input()
+if x == "Shrek":
+    Image = Image.open("Shreks.jpg")
+    print(Image.format, Image.size, Image.mode)
+    Image.show()
 
-size = (128, 128)
-
-for infile in sys.argv[1:]:
-    outfile = os.path.splitext(infile)[0] + ".thumbnail"
-    if infile != outfile:
-        try:
-            with Image.open(infile) as im:
-                im.thumbnail(size)
-                im.save(outfile, "JPEG")
-        except OSError:
-            print("cannot create thumbnail for", infile)
-
-box = (100, 100, 400, 400)
-region = im.crop(box)
-
-region = region.transpose(Image.Transpose.ROTATE_180)
-im.paste(region, box)
-
+y = input()
+if y == "Ronaldo":
+    Image2 = Image.open("Ronaldo.jpeg")
+    print(Image2.format, Image2.size, Image2.mode)
+    Image2.show()
